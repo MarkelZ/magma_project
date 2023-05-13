b6285ba752395f734c2ef40b5731234cd71661ce
Márk Széles <mszeles@lilo7.science.ru.nl>
2023-05-13 17:34:24 +0200

    FlipABPath

--- src/common.m
+++ src/common.m
@@ -47,3 +47,40 @@ function M(G,d,v)
   end for;
   return Mv;
 end function;
+
+// v is the starting point
+// v has an alpha-edge
+// w is the endpoint
+// we might have to check for infinite looping,
+// if this can run on cycles. I think it cannot.
+procedure FlipABPath(~G, v, alpha, beta, ~w)
+  currentVertex := v;
+  hasNext := true;
+  nextColour := alpha;
+  while hasNext do
+    // Find incident edge with label nextColour
+    es := [ e : e in IncidentEdges(currentVertex) | IsLabelled(e) and Label(e) eq nextColour ];
+    if #es eq 0 then
+      w := currentVertex;
+      hasNext := false;
+    else
+      // we assumed there is only one such e
+      e := es[1];
+      currentVertex := Random(Exclude(EndVertices(e),currentVertex));
+      if nextColour eq alpha then
+        nextColour := beta;
+      else
+        nextColour := alpha;
+      end if;
+    end if;
+  end while;
+
+end procedure;
+
+G := CompleteGraph(6);
+V := VertexSet(G);
+E := EdgeSet(G);
+w := -1;
+AssignLabel(E ! {1,2},1);
+AssignLabel(E ! {2,3},0);
+FlipABPath(~G,V ! 1,1,0,~w);
